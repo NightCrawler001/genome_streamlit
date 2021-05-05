6be96e93adc2ea5707585aea0dcc05fadd7360b9
DanShai <danshai2013@gmail.com>
2021-05-05 11:08:18 +0300

    05-05-21

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from gnet import Gnet
 import streamlit as st
 
-st.write("""
-# Genome network App
+
+st.write("""# Genome network App
 This app uses genomes instead of neurons to build the model!
 Genome Code origin  [Genome](https://github.com/DanShai/Genome) in python by @Danshai.
 """)
 
-st.header(' Genome App')
 gn = Gnet()
 st.sidebar.header('Set some Genome params')
 epochs = st.sidebar.selectbox('Epochs', list(range(100,1000,100)))
